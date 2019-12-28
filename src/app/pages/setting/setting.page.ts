af537320574bf338829e0b46011c2153c9ce1d3c
DogeZhang <zhangyz86@hotmail.com>
2019-12-28 23:12:19 +0800

    7. 添加返回按钮

--- src/app/pages/setting/setting.page.ts
+++ src/app/pages/setting/setting.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-setting',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.page.scss'],
 })
 export class SettingPage implements OnInit {
+  private VERSION: string = "1.8.3";
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController
+  ) { }
 
   ngOnInit() {
   }
-
+  goBack(){
+    this.navCtrl.back();
+  }
 }
